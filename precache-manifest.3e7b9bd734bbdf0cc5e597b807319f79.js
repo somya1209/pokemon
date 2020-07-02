c088c0baec0956878cf437eae7699addda9d414e
somya1209 <somya123024@gmail.com>
2020-07-02 19:43:21 +0530

    Updates

---
+++ precache-manifest.3e7b9bd734bbdf0cc5e597b807319f79.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "d55b753635306819dcab54afa42b2c4d",
+    "url": "/pokemon/index.html"
+  },
+  {
+    "revision": "559778c7545f3c8dd500",
+    "url": "/pokemon/static/css/main.a57ff972.chunk.css"
+  },
+  {
+    "revision": "7aab0cac9acbffdbea96",
+    "url": "/pokemon/static/js/2.d06ccdd7.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/pokemon/static/js/2.d06ccdd7.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "559778c7545f3c8dd500",
+    "url": "/pokemon/static/js/main.023f69e8.chunk.js"
+  },
+  {
+    "revision": "b931b09f9078cae05f94",
+    "url": "/pokemon/static/js/runtime-main.89aedc36.js"
+  }
+]);
